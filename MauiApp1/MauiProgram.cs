3c5fd284c236ebbb437a1e867556f1c2fa0d7b4d
Pepone <josesoftwarem@gmail.com>
2024-06-05 22:18:44 +0200

    Comentario para ver funcionamiento git

--- MauiApp1/MauiProgram.cs
+++ MauiApp1/MauiProgram.cs
@@ -13,6 +13,7 @@ namespace MauiApp1
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+					// prueba para git
                 });
 
 #if DEBUG
